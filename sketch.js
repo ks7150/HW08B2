baed37ab9a93fca10dea5700ad70e58b53325b19
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-10-31 14:37:36 -0400

    HW08B line

--- sketch.js
+++ sketch.js
@@ -1,6 +1,48 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+let song; 
+
+let peaks;
+
+let sliderGraph; 
+
+function preload() {
+  song = loadSound("autumn-leaf_with-violins-74917.mp3");
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+peaks = song.getPeaks(width)
+
+
 }
+
+
+
+function draw() {
+  background(120, 120, 220);
+for(let i=0; i < peaks.length; i++){
+  
+line (i%width,height/2+peaks[i]*height/2, i%width, height/2)
+let x= i%width;
+noFill()
+strokeWeight(1/2)
+
+}
+
+
+
+
+}
+
+function mouseClicked() {
+  song.play();
+}
+
+
+
+
+
+
+
+
+
