2cfcdc22e1b5e380cf3d9528b8ddfd2720a10bb4
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-10-31 14:50:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let song;
 
 let peaks;
 
-let sliderGraph; 
+let link; 
 
 function preload() {
   song = loadSound("autumn-leaf_with-violins-74917.mp3");
@@ -12,7 +12,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 peaks = song.getPeaks(width)
-
+link= createA("https://ks7150.github.io/HW08B/","Ellipses")
 
 }
 
@@ -24,8 +24,7 @@ for(let i=0; i < peaks.length; i++){
   
 line (i%width,height/2+peaks[i]*height/2, i%width, height/2)
 let x= i%width;
-noFill()
-strokeWeight(1/2)
+link.position(10,10);
 
 }
 
